84c880c48bc584c7932030ebf9a6dab159efad1a
Pankaj086 <pankajkumar086420@gmail.com>
2025-01-25 22:21:59 +0530

    Updated NextConfig.ts

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,27 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images:{
-    domains:['images.unsplash.com','res.cloudinary.com']
-  }
+  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'assets.aceternity.com',
+        pathname: '**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        pathname: '**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '**',
+      },
+
+    ],
+  },
 };
 
 export default nextConfig;
