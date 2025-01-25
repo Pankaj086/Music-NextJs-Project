141a1ce93943b5fe09e3e0cf83f195ca69d46c80
Pankaj086 <pankajkumar086420@gmail.com>
2025-01-25 22:34:41 +0530

    Updated UI files

--- src/components/ui/3d-card.tsx
+++ src/components/ui/3d-card.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { cn } from "@/lib/utils";
-import Image from "next/image";
+
 import React, {
     createContext,
     useState,
@@ -35,12 +35,12 @@ import React, {
         containerRef.current.style.transform = `rotateY(${x}deg) rotateX(${y}deg)`;
     };
 
-    const handleMouseEnter = (e: React.MouseEvent<HTMLDivElement>) => {
+    const handleMouseEnter = () => {
         setIsMouseEntered(true);
         if (!containerRef.current) return;
     };
 
-    const handleMouseLeave = (e: React.MouseEvent<HTMLDivElement>) => {
+    const handleMouseLeave = () => {
         if (!containerRef.current) return;
         setIsMouseEntered(false);
         containerRef.current.style.transform = `rotateY(0deg) rotateX(0deg)`;
